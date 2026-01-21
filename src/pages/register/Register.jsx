e2734f316d64217b8b7309d9ff8f0e2413d5151d
bharatroy4D <bharatroy1005@gmail.com>
2026-01-21 12:31:29 +0600

    Educational project setup

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1 className='text-emerald-600 font-bold text-center text-4xl py-4'>This is Register</h1>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/route/route.jsx
@@ -0,0 +1,39 @@
+import Root from "../layout/Root";
+import Abouts from "../pages/abouts/Abouts";
+import Contacts from "../pages/contacts/Contacts";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+            {
+                path: "/",      // Home
+                element: <Home />
+            },
+            {
+                path: "abouts",  // 👉 /about
+                element: <Abouts />
+            },
+            {
+                path: "contacts", // 👉 /contact
+                element: <Contacts />
+            },
+            {
+                path: "login",   // 👉 /login
+                element: <Login />
+            },
+            {
+                path: "register", // 👉 /register
+                element: <Register />
+            },
+        ]
+    }
+]);
+
+export default router;
